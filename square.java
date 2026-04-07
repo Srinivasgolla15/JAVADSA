000023fcd41a425a17f0e03465981d67f7054d9d
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-04-07 21:39:04 +0530

    java basecs

---
+++ square.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int square = a*a;
+        System.out.println(square);
+    }}
+
