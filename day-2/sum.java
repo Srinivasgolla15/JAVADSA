8f1e7350be94f145c1025d6c807bc17cda57f7c5
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-04-18 17:38:04 +0530

    while loop

---
+++ day-2/sum.java
@@ -0,0 +1,11 @@
+public class sum{
+    public static void main(String[] args){
+        int sum=0;
+        int n=5;
+        while(n>0){
+            sum+=n;
+            n--;
+        }
+        System.out.println(sum);
+    }
+}
