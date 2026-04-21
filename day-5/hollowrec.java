ae1ba63586b1c9fe95b1a14ff90a3c812fc411cc
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-04-21 07:58:25 +0530

    hollowrectagle

---
+++ day-5/hollowrec.java
@@ -0,0 +1,17 @@
+public class hollowrec {
+    public static void main(String[] args) {
+        int rows = 4;
+        int cols = 5;
+        for (int i =0;i<rows;i++){
+            for (int j =0;j<cols;j++){
+                if (i==0 || i==rows-1 || j==0 || j==cols-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
